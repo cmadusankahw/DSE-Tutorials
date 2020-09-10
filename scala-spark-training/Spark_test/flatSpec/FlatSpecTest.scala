475374cd9a4bec99fc10827d1fbac15924c2ed75
cmadusankahw <madushan_im16046@stu.kln.ac.lk>
2020-09-10 11:19:38 +0530

    Sample Unit test cases in FunSuite and FlatSpec exercises initial commit

---
+++ scala-spark-training/Spark_test/flatSpec/FlatSpecTest.scala
@@ -0,0 +1,18 @@
+package Spark_test.flatSpec
+
+import org.scalatest.FlatSpec
+
+object FlatSpecTest  extends FlatSpec  {
+  def main(args: Array[String]): Unit = {
+    "An empty List" should "have size 0" in {
+      assert(List.empty.size == 0)
+    }
+
+    it should "throw an IndexOutOfBoundsException when trying to access any element" in {
+      val emptyList = List();
+      assertThrows[IndexOutOfBoundsException] {
+        emptyList(1)
+      }
+    }
+  }
+}
